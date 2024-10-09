f0536d15c237fb0a862869e32d72304335617127
Frank Cichos <cichos@physik.uni-leipzig.de>
2024-10-09 13:09:51 +0200

    Lecture 1

--- source/conf.py
+++ source/conf.py
@@ -16,6 +16,7 @@ extensions = [
     'nbsphinx',
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
+    'jupyterlite_sphinx',
     # Removed 'furo.sphinxext' as it's not a valid extension
 ]
 
--- source/index.rst
+++ source/index.rst
@@ -1,3 +1,7 @@
+---
+hide-toc: true
+---
+
 .. Computational Software documentation master file, created by
    sphinx-quickstart on Tue Mar 31 12:45:28 2020.
    You can adapt this file completely to your liking, but it should at least
